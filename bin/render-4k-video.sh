a56893870ac858174392b5255c44008ca21cf927
Robin Houston <robin.houston@gmail.com>
2014-01-03 22:55:28 +0000

    Render 4k vid at 30 fps

--- bin/render-4k-video.sh
+++ bin/render-4k-video.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+FPS=30
+
 MAPS=(_raw Area Population GDP Extraction Emissions Consumption Historical Reserves PeopleAtRisk SeaLevel Poverty)
 
 i=0
@@ -12,18 +14,18 @@ do
     to=${MAPS[$i+1]}
     
     j=0
-    while [ $j -le 24 ]
+    while [ $j -lt $FPS ]
     do
         filename="4k-video/$to-$(printf %02d $j)"
         if [ ! -e "$filename-full.png" ]
         then
             echo >&2 "Rendering $filename..."
-            t=$(bc <<< "scale=2; $j/24")
+            t=$(bc <<< "scale=2; $j/($FPS-1)")
             webkit2png -F "http://carbonmap.local/massive.xhtml#$from/$to/$t" -o "$filename"
         fi
         j=$[$j+1]
     done
     
-    ffmpeg -y -i 4k-video/"$to"-%02d-full.png -vcodec prores -vf "pad=3840:2160:0:0:white" -bufsize 4000k 4k-video/"$to".mov
+    ffmpeg -y -i 4k-video/"$to"-%02d-full.png -r $FPS -vcodec prores -vf "pad=3840:2160:0:0:white" -bufsize 4000k 4k-video/"$to".mov
     i=$[$i+1]
 done
