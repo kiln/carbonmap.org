6001f8a55f4f962e8bf8e3af70f9fbb3a0f28b58
Robin Houston <robin.houston@gmail.com>
2014-09-19 16:44:46 +0100

    Extract latest year from World Bank data for Maps

---
+++ bin/prepare-data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Convert the raw spreadsheets into a form and location appropriate
+# for load-data and load-shading
+
+WB_LATEST=/usr/local/cartograms/bin/wb-latest.py
+WB_DATA="data/Raw/World Bank"
+POPMULT="--multiply-by=$WB_DATA/sp.pop.totl_Indicator_en_csv_v2.csv"
+
+"$WB_LATEST"            "$WB_DATA"/ag.lnd.totl.k2_Indicator_en_csv_v2.csv    > data/Maps/Area.csv
+"$WB_LATEST"            "$WB_DATA"/ny.gdp.mktp.pp.cd_Indicator_en_csv_v2.csv > data/Maps/GDP.csv
+"$WB_LATEST" "$POPMULT" "$WB_DATA"/en.clc.mdat.zs_Indicator_en_csv_v2.csv    > data/Maps/PeopleAtRisk.csv
+"$WB_LATEST"            "$WB_DATA"/sp.pop.totl_Indicator_en_csv_v2.csv       > data/Maps/Population.csv
+"$WB_LATEST" "$POPMULT" "$WB_DATA"/si.pov.dday_Indicator_en_csv_v2.csv       > data/Maps/Poverty.csv
+"$WB_LATEST" "$POPMULT" "$WB_DATA"/en.pop.el5m.zs_Indicator_en_csv_v2.csv    > data/Maps/SeaLevel.csv
+
