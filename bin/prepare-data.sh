9aaa939155b19345774d785df59401d5aa450b45
Robin Houston <robin.houston@gmail.com>
2014-09-19 18:44:05 +0100

    More data
    Add Consumption and Emissions datasets from Glen Peters,
    and add alpha-2 codes to the World Bank datasets.

--- bin/prepare-data.sh
+++ bin/prepare-data.sh
@@ -9,7 +9,7 @@
 WB_LATEST=/usr/local/cartograms/bin/wb-latest.py
 WB_DATA="data/Raw/World Bank"
 POPMULT="--multiply-by=$WB_DATA/sp.pop.totl_Indicator_en_csv_v2.csv"
-
+ECN="/usr/local/cartograms/bin/encode-country-names.py"
 
 # World Bank data
 "$WB_LATEST"            "$WB_DATA"/ag.lnd.totl.k2_Indicator_en_csv_v2.csv    > data/Maps/Area.csv
@@ -22,4 +22,19 @@ POPMULT="--multiply-by=$WB_DATA/sp.pop.totl_Indicator_en_csv_v2.csv"
 "$WB_LATEST"            "$WB_DATA"/ny.gdp.pcap.pp.cd_Indicator_en_csv_v2.csv > data/Shading/GDPperCapita.csv
 "$WB_LATEST"            "$WB_DATA"/sp.pop.grow_Indicator_en_csv_v2.csv       > data/Shading/PopulationGrowth.csv
 
+for dataset in Area GDP PeopleAtRisk PopulationGrowth Poverty SeaLevel
+do
+	"$ECN" data/Maps/"$dataset".csv > "data/Maps/With alpha-2/$dataset".csv
+done
+
+for dataset in GDPperCapita PopulationGrowth
+do
+	"$ECN" data/Shading/"$dataset".csv > "data/Shading/With alpha-2/$dataset".csv
+done
+
+# GCB from Glen Peters
+bin/convert-gcb.py --conversion-factor=3.664 "data/Raw/GCB from Glen Peters/Global_Carbon_Budget_2014_v1.0/Territorial Emissions.csv" > data/Maps/Emissions.csv
+bin/convert-gcb.py --conversion-factor=3.664 --year=2012 data/Raw/GCB\ from\ Glen\ Peters/Global_Carbon_Budget_2014_v1.0/Consumption\ Emissions.csv > data/Maps/Consumption.csv
+
+# bin/csv-ratio.py data/Maps/Emissions.csv data/Maps/Population.csv > data/Shading/CO2perCapita.csv
 
