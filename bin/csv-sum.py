1774691babe78438214b54a422489dda8967cec5
Robin Houston <robin.houston@gmail.com>
2014-09-22 16:11:11 +0100

    Finish data update

--- bin/csv-sum.py
+++ bin/csv-sum.py
@@ -25,6 +25,8 @@ def each_csv_row(filename, ignore_rows=0):
 
 
 parser = optparse.OptionParser(usage="%prog [options] file.csv file.csv ...")
+parser.add_option("", "--multipliers", type="str", default=None,
+                help="Constant factors to multiply inputs by, for unit conversion, comma-separated")
 parser.add_option("", "--scale", type="float", default=1.0,
                 help="Constant factor to multiply result by, for unit conversion")
 parser.add_option("", "--key", type="str", default="Alpha-2",
@@ -34,14 +36,21 @@ parser.add_option("", "--value", type="str", default="Value",
 
 (options, args) = parser.parse_args()
 
+if options.multipliers:
+	multipliers = map(float, options.multipliers.split(","))
+	if len(multipliers) != len(args):
+		parser.error("The number of multipliers should match the number of input files")
+else:
+	multipliers = [1.0] * len(args)
+
 sums = {}
 keys = set()
-for filename in args[:-1]:
+for multiplier, filename in zip(multipliers, args[:-1]):
 	for d in each_csv_row(filename):
 		key = d[options.key]
 		value = d[options.value]
 		if value:
-			sums[key] = sums.get(key, 0.0) + float(value)
+			sums[key] = sums.get(key, 0.0) + float(value) * multiplier
 			keys.add(key)
 
 with open(args[-1], 'r') as f:
@@ -55,7 +64,7 @@ with open(args[-1], 'r') as f:
 		key = d[options.key]
 		value = d[options.value]
 		if value:
-			value = (float(value) + sums.get(key, 0.0)) * options.scale
+			value = (float(value) + sums.get(key, 0.0) * multipliers[-1]) * options.scale
 		keys.discard(key)
 
 		w.writerow([ key, value ])
--- bin/prepare-data.sh
+++ bin/prepare-data.sh
@@ -45,26 +45,33 @@ bin/csv-ratio.py --scale=1e6 data/Maps/"With alpha-2"/Emissions.csv data/Maps/"W
 # Already has alpha-2 codes, so no need to reencode
 cp data/Shading/CO2perCapita.csv data/Shading/"With alpha-2"/CO2perCapita.csv
 
+bin/emissions-change.py data/Raw/GCB\ from\ Glen\ Peters/Global_Carbon_Budget_2014_v1.0/Territorial\ Emissions.csv > data/Shading/EmissionsChange.csv
+"$ECN" data/Shading/EmissionsChange.csv > data/Shading/"With alpha-2"/EmissionsChange.csv
+
 
 ## BP data
 # Extraction
-bin/convert-bp.py --conversion-factor=1.0 data/Raw/BP/BP-Statistical_Review_of_world_energy_2014_workbook/Oil\ Production\ –\ Tonnes.csv > data/Working/Extraction-oil.csv
-bin/convert-bp.py --conversion-factor=1.0 data/Raw/BP/BP-Statistical_Review_of_world_energy_2014_workbook/Gas\ Production\ –\ tonnes.csv > data/Working/Extraction-gas.csv
-bin/convert-bp.py --conversion-factor=1.0 data/Raw/BP/BP-Statistical_Review_of_world_energy_2014_workbook/Coal\ Production\ -\ Tonnes.csv > data/Working/Extraction-coal.csv
+bin/convert-bp.py --conversion-factor=$(bc -l <<<"0.1364 * 3.07") data/Raw/BP/BP-Statistical_Review_of_world_energy_2014_workbook/Oil\ Production\ –\ Tonnes.csv > data/Working/Extraction-oil.csv
+bin/convert-bp.py --conversion-factor=$(bc -l <<<"0.90 * 2.35") data/Raw/BP/BP-Statistical_Review_of_world_energy_2014_workbook/Gas\ Production\ –\ tonnes.csv > data/Working/Extraction-gas.csv
+bin/convert-bp.py --conversion-factor=$(bc -l <<<"0.513 * 3.96") data/Raw/BP/BP-Statistical_Review_of_world_energy_2014_workbook/Coal\ Production\ -\ Tonnes.csv > data/Working/Extraction-coal.csv
 
 bin/csv-sum.py --key="Country Name" data/Working/Extraction-oil.csv data/Working/Extraction-gas.csv data/Working/Extraction-coal.csv > data/Maps/Extraction.csv
+"$ECN" data/Maps/Extraction.csv > data/Maps/"With alpha-2"/Extraction.csv
 
 # Reserves
 /usr/local/cartograms/bin/csv-snip --expect-header="Oil: Proved reserves,at end 2013" --output-header="Country Name,Value" --cols=A,F --data-rows=6..70 'data/Raw/BP/BP-Statistical_Review_of_world_energy_2014_workbook/Oil – Proved reserves.csv'  > data/Working/Reserves-oil.csv
 /usr/local/cartograms/bin/csv-snip --header-row=2 --expect-header=",at end 2013" --output-header="Country Name,Value" --cols=A,F --data-rows=7..72 data/Raw/BP/BP-Statistical_Review_of_world_energy_2014_workbook/Gas\ –\ Proved\ reserves.csv > data/Working/Reserves-gas.csv
 /usr/local/cartograms/bin/csv-snip --header-row=3 --expect-header="Million tonnes,Total" --output-header="Country Name,Value" --cols=A,D --data-rows=5..50 data/Raw/BP/BP-Statistical_Review_of_world_energy_2014_workbook/Coal\ -\ Reserves.csv > data/Working/Reserves-coal.csv
-
-# TODO multiply by conversion factors
-bin/csv-sum.py --key="Country Name" data/Working/Reserves-oil.csv data/Working/Reserves-gas.csv data/Working/Reserves-coal.csv > data/Maps/Reserves.csv
-
-# Add alpha-2 codes to the new files
-for dataset in Extraction Reserves
+for fuel in oil gas coal
 do
-	"$ECN" data/Maps/"$dataset".csv > "data/Maps/With alpha-2/$dataset".csv
+	"$ECN" data/Working/Reserves"-$fuel".csv > data/Working/"With alpha-2"/Reserves"-$fuel".csv
 done
 
+# Convert all values into millions of tonnes of CO₂
+bin/csv-sum.py --key="Alpha-2" --multipliers=$(bc -l <<<"0.1364 * 3.07 * 0.001"),$(bc -l <<<"0.90 * 2.35"),$(bc -l <<<"0.513 * 3.96") data/Working/"With alpha-2"/Reserves-{oil,gas,coal}.csv > data/Maps/Reserves.csv
+cp data/Maps/Reserves.csv data/Maps/"With alpha-2"/Reserves.csv
+
+
+# Historical
+perl -ln -e 's/Emissions 1850 to 2011/Value/; @F=split(/\t/,$_); print "\"$F[0]\",$F[1]"' data/Raw/CAIT/Historical.tsv > data/Maps/Historical.csv
+"$ECN" data/Maps/Historical.csv > data/Maps/"With alpha-2"/Historical.csv
